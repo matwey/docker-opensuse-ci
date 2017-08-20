a88da92cd7c36ef9c6a79d3cd4fccfa7fdd51ba7
Matwey V. Kornilov <matwey.kornilov@gmail.com>
2017-08-20 21:17:24 +0300

    Add clang4 compiler

--- generator.py
+++ generator.py
@@ -22,6 +22,22 @@ class Image(object):
 		else:
 			raise NotImplementedError()
 
+	def clang_repository(self):
+		if self.suseversion == -1:
+			return "http://download.opensuse.org/repositories/devel:/tools:/compiler/openSUSE_Factory/devel:tools:compiler.repo"
+		elif self.suseversion == 422:
+			return "http://download.opensuse.org/repositories/devel:/tools:/compiler/openSUSE_Leap_42.2/devel:tools:compiler.repo"
+		else:
+			raise NotImplementedError()
+
+	def compiler_repository(self):
+		if self.compiler[0] == 'gcc':
+			return self.gcc_repository()
+		elif self.compiler[0] == 'clang':
+			return self.clang_repository()
+		else:
+			raise NotImplementedError()
+
 	def qt_repository(self):
 		factory_name = 'Tumbleweed'
 		if self.qt == 55:
@@ -70,6 +86,15 @@ def generic_gcc(image, m):
 	image.packages += ['cmake','make','libQt5Widgets-devel','libQt5Test-devel','libQt5Gui-devel','libQt5Core-devel']
 	image.packages += ['gcc{}-c++'.format(gcc_suffix_without_dot),]
 
+def generic_clang(image, m):
+	clang_suffix = m.group(1)
+	clang_suffix_without_dot = clang_suffix.replace(".","")
+	qt_suffix = m.group(2)
+	image.env['CC']  = 'clang-' + clang_suffix
+	image.env['CXX'] = 'clang++-' + clang_suffix
+	image.packages += ['cmake','make','libQt5Widgets-devel','libQt5Test-devel','libQt5Gui-devel','libQt5Core-devel']
+	image.packages += ['clang{}'.format(clang_suffix_without_dot),]
+
 def requires_leap_422(image, m):
 	if image.suseversion > 422 or image.suseversion == -1:
 		image.suseversion = 422
@@ -81,14 +106,15 @@ def requires_leap_421(image, m):
 g = Generator()
 
 g.addRule("gcc(.*?)-qt(.*)", generic_gcc)
+g.addRule("clang(.*?)-qt(.*)", generic_clang)
 g.addRule("gcc4.8-qt(.*)", requires_leap_422)
-g.addRule("gcc(.*?)-qt5([67])", requires_leap_422)
+g.addRule("(.*?)-qt5([67])", requires_leap_422)
 g.addRule("gcc4.8-qt55", requires_leap_421)
 
 env = Environment(loader=FileSystemLoader('.'),trim_blocks=True)
 template = env.get_template('Dockerfile.Jinja2')
 
-for (compiler,version) in [("gcc",7),("gcc",6),("gcc","4.8")]:
+for (compiler,version) in [("gcc",7),("gcc",6),("gcc","4.8"),("clang","4")]:
 	for qt in [55,56,57,58,59]:
 		im = Image(suseversion = -1, compiler = (compiler, version), qt = qt)
 		g.match(im, "{}{}-qt{}".format(compiler,version,qt))
@@ -96,7 +122,7 @@ for (compiler,version) in [("gcc",7),("gcc",6),("gcc","4.8")]:
 		kwargs = {}
 		kwargs['baseimage'] = im.base_image()
 		kwargs['env']       = [{"key" : k, "value" : v} for (k,v) in im.env.items()]
-		kwargs['repos']     = [{"value": im.gcc_repository()}, {"value": im.qt_repository()}]
+		kwargs['repos']     = [{"value": im.compiler_repository()}, {"value": im.qt_repository()}]
 		kwargs['packages']  = im.packages
 
 		filename = im.output_file()
